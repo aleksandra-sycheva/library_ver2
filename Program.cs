9fa08004d6f8cc7528ef112277afcea49d6cf71b
Александра Сычева <sashasychevaisip@gmail.com>
2026-03-26 15:14:46 +0500

    реализован вход

--- Program.cs
+++ Program.cs
@@ -1,17 +1,41 @@
-namespace library_ver2
+using library;
+using library_ver2;
+
+namespace library
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new FormLogin());
+            bool exitProgram = false;
+
+            while (!exitProgram)
+            {
+                using (var formLogin = new FormLogin())
+                {
+                    if (formLogin.ShowDialog() == DialogResult.OK)
+                    {
+                        using (var formProducts = new FormBooks(
+                            formLogin.CurrentUser,
+                            formLogin.IsGuest))
+                        {
+                            if (formProducts.ShowDialog() == DialogResult.Cancel)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                exitProgram = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        exitProgram = true;
+                    }
+                }
+            }
         }
     }
 }
